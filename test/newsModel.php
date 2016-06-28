933aa9af52a0f36a610c78d537b5737e8026aa17
RAJEN <rajenshrestha5>
2016-06-28 14:35:14 +0545

    cxgvgf

--- test/newsModel.php
+++ test/newsModel.php
@@ -6,7 +6,7 @@ require 'Model/News.php';
 
 $data = [
     'title' => 'Lorem ipsum',
-    'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A consequatur dicta, dignissimos dolor dolore dolorum in libero maxime molestias mollitia perferendis quasi ratione rem reprehenderit sequi voluptas voluptates! Itaque, veritatis'
+    'content' => ' Lorem ipsum dolor sit amet, consectetur adipisicing elit. A consequatur dicta, dignissimos dolor dolore dolorum in libero maxime molestias mollitia perferendis quasi ratione rem reprehenderit sequi voluptas voluptates! Itaque, veritatis'
 ];
 
 $user = new \Model\News;
